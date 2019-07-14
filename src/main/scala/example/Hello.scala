c79b6781bb406148fb7d318aa9e3c6495d31dc13
Siddharth Chauhan <csiddha@amazon.com>
2019-07-14 15:59:34 -0700

    Add tests for take, drop, takeWhile first pass

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,5 +1,91 @@
 package example
+import Stream._
 
 object Hello extends App {
   println("Hello")
+}
+
+trait Stream[+A] {
+  /*
+    Write a function to convert a Stream to a List, which will force its evaluation and let you look at it in the REPL. You can convert to the regular List type in the standard library. You can place this and other functions that operate on a Stream inside the Stream trait.
+    def toList: List[A]
+  */
+  def toList(): List[A] = this match {
+    case Empty => List[A]()
+    case Cons(h, t) => return (h() :: t().toList())
+  }
+
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if(n > 1)=> { return cons(h(), t().take(n-1)) } 
+    case Cons(h, _) if(n==1) => { return cons(h(), empty) }
+    case _ => empty 
+  }
+
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(h, t) if(n >= 1) => { 
+          return t().drop(n-1)
+    }
+    case Cons(h, t) if(n == 0) => { 
+          return Cons(h, t) 
+    }
+    case _ => { 
+      empty
+    }
+  }
+
+
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) => {
+      if(p(h())) {
+        return cons(h(), t().takeWhile(p))
+      } else {
+        return t().takeWhile(p) 
+      }
+    }
+    case _ => empty
+  }
+
+  def forAll(p: A => Boolean): Boolean = ???
+
+  def headOption: Option[A] = ???
+
+  // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
+  // writing your own function signatures.
+
+  def startsWith[B](s: Stream[B]): Boolean = ???
+}
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  /*
+  *
+     We typically want to cache the values of a Cons node, once they are forced. If we use the Cons data constructor directly, for instance, this code will actually compute expensive(x) twice: 
+
+     val x = Cons(() => expensive(x), tl) val h1 = x.headOption
+     val h2 = x.headOption
+
+    We typically avoid this problem by defining smart constructors, which is what we call a function for constructing 
+    a data type that ensures some additional invariant or provides a slightly different signature than 
+    the “real” constructors used for pattern matching.
+
+    By convention, smart constructors typically lowercase the first letter of the corresponding data constructor.
+
+    Here, our cons smart constructor takes care of memoizing the by-name arguments for the head and tail of the Cons.
+    This is a common trick, and it ensures that our thunk will only do its work once, when forced for the first time.
+    Subsequent forces will return the cached lazy val.
+  */
+
+
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A] (as: A*): Stream[A] =
+    if(as.isEmpty) empty
+    else cons(as.head, apply(as.tail: _*))
 }
--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -1,7 +1,59 @@
+package example
 import org.scalatest._
 
 class HelloSpec extends FunSuite with DiagrammedAssertions {
   test("Hello should start with H") {
     assert("Hello".startsWith("H"))
   }
+
+  test("testToList with normal values") {
+    val me = Stream(1, 2, 3, 4, 5)
+    val expected = List(1, 2, 3, 4, 5)
+    val actual = me.toList
+    assert(actual == expected)
+	}
+
+  test("testToList with lazy values") {
+    val me = Stream({println("hi1"); 0 + 1},
+                    {println("hi2"); 0 + 2},
+                    {println("hi3"); 0 + 3},
+                    {println("hi4"); 0 + 4})
+    val expected = List(1, 2, 3, 4)
+    println("after Stream and before toList")
+    val actual = me.toList
+    assert(actual == expected)
+  }
+
+  test("drop test") {
+    val me = Stream({ 0 + 1},
+                    { 0 + 2},
+                    { 0 + 3},
+                    { 0 + 4});
+    val expected = me.drop(2).toList;
+    assert(expected == List(3, 4));
+  }
+
+  test("takeWhile test") {
+    val me = Stream({1},{2},{3},{4});
+    val p = (x: Int) => x % 2 != 0;
+    val actual = me.takeWhile(p).toList;
+    val expected = List(1,3);
+    assert(expected == actual);
+  }
+  
+  test("take test") { val me = Stream({ 0 + 1},
+                    { 0 + 2},
+                    { 0 + 3},
+                    { 0 + 4});
+    val expected = me.take(2).toList;
+    assert(expected == List(1,2));
+  }
+
+	test("takeWhile normal values") {
+    val me = Stream({println("hi"); 1}, {println("hi"); 2}, 3, 4)
+    val p = (x: Int) => x < 3
+    val expected = Stream(1, 2).toList
+    val actual = me.takeWhile(p).toList
+    assert(actual == expected)
+	}
 }
